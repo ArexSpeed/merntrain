0c7badc7e56c7a7a39ccb2310a767361da9a1a01
Arek Cichocki <arexspeed95@gmail.com>
2021-01-30 20:09:06 +0100

    add context to components & count balance

--- expenser/client/src/components/TransactionList.js
+++ expenser/client/src/components/TransactionList.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalContext } from "../context/GlobalState";
+import Transaction from './Transaction';
 
 const TransactionList = () => {
+  const {transactions} = useContext(GlobalContext);
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        Transaction List
+      {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </>
   )
