44d0ef8896b318b888d11a5e6ccf926ac66d55b6
Arek Cichocki <arexspeed95@gmail.com>
2021-01-30 23:21:21 +0100

    change static to backend in files & finish

--- expenser/client/src/context/GlobalState.js
+++ expenser/client/src/context/GlobalState.js
@@ -19,7 +19,7 @@ export const GlobalProvider = ({ children }) => {
   //Actions 
   async function getTransactions() {
     try {
-      const res = await axios.get('/api/v1/transactions');
+      const res = await axios.get('/api/transactions');
 
       dispatch({
         type: 'GET_TRANSACTIONS',
