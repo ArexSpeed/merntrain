43ab76c5587481da1db71132b670a92cab5b6b18
Arek Cichocki <arexspeed95@gmail.com>
2021-01-30 23:23:07 +0100

    code to split front&back on host

--- expenser/server/server.js
+++ expenser/server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import colors from 'colors';
 import morgan from 'morgan';
 import transactions from './routes/transactions.js'
@@ -13,6 +14,13 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+
+//If I want to split frontend and backend on one host
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.use('/api/transactions', transactions)
 
