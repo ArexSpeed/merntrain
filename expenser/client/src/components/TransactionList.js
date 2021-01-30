c98a96b889793db80fa57d7a355b37eee39f5c1e
Arek Cichocki <arexspeed95@gmail.com>
2021-01-30 19:51:23 +0100

    make raw components

---
+++ expenser/client/src/components/TransactionList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TransactionList = () => {
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        Transaction List
+      </ul>
+    </>
+  )
+}
+
+export default TransactionList
