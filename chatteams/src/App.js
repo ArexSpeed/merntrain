db12fd172f2e4dea47647ff423bec54d4d00b12d
Arek Cichocki <arexspeed95@gmail.com>
2021-02-02 06:45:26 +0100

    add rest components

--- chatteams/src/App.js
+++ chatteams/src/App.js
@@ -1,14 +1,17 @@
 import {ChatEngine} from 'react-chat-engine'
 import './App.css';
 import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm';
 
+const projectID = "e178f79b-0554-4d88-b050-7a84b0470424"
 function App() {
+  if (!localStorage.getItem('username')) return <LoginForm />;
   return (
     <ChatEngine 
       height='100vh'
-      projectID="e178f79b-0554-4d88-b050-7a84b0470424"
-      userName="tommy"
-      userSecret="tommy098"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
     />
    
