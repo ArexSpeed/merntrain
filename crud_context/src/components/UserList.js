53920a385c05f9431727ded6a3956192de16034a
Arek Cichocki <arexspeed95@gmail.com>
2021-02-06 21:41:01 +0100

    init context (global state) and display in list

--- crud_context/src/components/UserList.js
+++ crud_context/src/components/UserList.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import {useContext} from 'react'
+import {GlobalContext} from '../context/GlobalState'
 import {Link} from 'react-router-dom'
 import {ListGroup, ListGroupItem, Button} from 'reactstrap'
 
 const UserList = () => {
+  const {users} = useContext(GlobalContext)
+  console.log(users)
   return (
     <ListGroup>
-      <ListGroupItem className="d-flex">User One
+      {users.map((user, index) => (
+        <ListGroupItem className="d-flex" key={index}>{user.name}
       <div className="ml-auto">
-        <Link to='/edit/1' className="btn btn-warning mr-1">Edit</Link>
+        <Link to={`/edit/${user.id}`} className="btn btn-warning mr-1">Edit</Link>
         <Button color="danger">Delete</Button>
       </div>
       </ListGroupItem>
+      ))}
+      
 
     </ListGroup>
   )
---
+++ crud_context/src/context/GlobalState.js
@@ -0,0 +1,24 @@
+import {createContext, useReducer} from 'react'
+import AppReducer from './AppReducer'
+
+const initialState = {
+  users: [
+    {id: 1, name: 'User One'},
+    {id: 2, name: 'User Two'},
+    {id: 3, name: 'User Three'},
+  ]
+}
+
+export const GlobalContext = createContext(initialState);
+
+export const GlobalProvider = ({children}) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState)
+
+  return (
+    <GlobalContext.Provider value={{
+      users: state.users
+    }}>
+      {children}
+    </GlobalContext.Provider>
+  )
+}
