f6dee58d797bf6b5de727f06fb297cf8657d41ac
Arek Cichocki <arexspeed95@gmail.com>
2021-02-07 21:20:07 +0100

    client structure

--- memories_posts/client/src/App.js
+++ memories_posts/client/src/App.js
@@ -1,11 +1,30 @@
-
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import './App.css';
+import memories from './images/memories.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
 
 function App() {
   return (
-    <div className="App">
-        Post
-    </div>
+    <Container maxWidth="lg">
+        <AppBar position="static" color="inherit">
+          <Typography variant="h2" align="center">Posts</Typography>
+          <img src={memories} alt="memories" height="60" />
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+    </Container>
   );
 }
 
---
+++ memories_posts/client/src/components/Posts/Post/Post.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div>
+       <p>Post</p>
+      <p>Post</p>
+    </div>
+  )
+}
+
+export default Post
