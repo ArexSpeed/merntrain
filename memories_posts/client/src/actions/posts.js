3417ef211be459b6e63f6b105d4078045fd3c99b
Arek Cichocki <arexspeed95@gmail.com>
2021-02-07 21:55:41 +0100

    start fetch from useSelector

--- memories_posts/client/src/actions/posts.js
+++ memories_posts/client/src/actions/posts.js
@@ -2,8 +2,11 @@ import * as api from '../api'
 
 //Action creators -> redux thunk allow as make async action and dispatch(action) instead return
 export const getPosts = () => async (dispatch) => {
+  try {
+    const {data} = await api.fetchPosts()
+    dispatch({type: 'FETCH_ALL', payload: []})
+  } catch (error) {
+    console.log(error.message)
+  }
 
-  const action = {type: 'FETCH_ALL', payload: []}
-
-  dispatch(action)
 }
