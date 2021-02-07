9199223ef96c1f580e89e54ae94bc61068d2fdf6
Arek Cichocki <arexspeed95@gmail.com>
2021-02-07 17:38:03 +0100

    first routes

--- memories_posts/server/index.js
+++ memories_posts/server/index.js
@@ -2,3 +2,23 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv';
+import connectDB from './db.js';
+import postRoutes from './routes/posts.js'
+
+const app = express();
+dotenv.config();
+connectDB();
+
+app.use('/posts', postRoutes)
+
+// set limit in send file from req (fe img)
+app.use(bodyParser.json({limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
