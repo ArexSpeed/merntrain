2327b0649f0800df8acb3f4bd4eccbe2b719c11d
Arek Cichocki <arexspeed95@gmail.com>
2021-02-08 18:53:56 +0100

    delete post

--- memories_posts/client/src/components/Posts/Post/Post.js
+++ memories_posts/client/src/components/Posts/Post/Post.js
@@ -5,10 +5,13 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
+import {useDispatch} from 'react-redux'
+import {deletePost} from '../../../actions/posts'
 
 
 
 const Post = ({post, setCurrentId}) => {
+  const dispatch = useDispatch()
   const classes = useStyles();
   return (
     <Card className={classes.card}>
@@ -29,15 +32,16 @@ const Post = ({post, setCurrentId}) => {
       <div className={classes.details}>
       <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => ` #${tag}`)}</Typography>
       </div>
-      <CardContent>
       <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
+      <CardContent>
+      <Typography variant="h5" gutterBottom>{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
         <Button size="small" color="primary" onClick={() => {}}>
           <ThumbUpAltIcon fontSize='small' />
           {" "} Like {" "} {post.likeCount}
         </Button>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize='small' />
           Delete
         </Button>
