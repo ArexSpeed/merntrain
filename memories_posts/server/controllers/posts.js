6bff73d2a6341fac3179ef71fecb9de640c7e19b
Arek Cichocki <arexspeed95@gmail.com>
2021-02-09 12:51:53 +0100

    like post

--- memories_posts/server/controllers/posts.js
+++ memories_posts/server/controllers/posts.js
@@ -46,3 +46,16 @@ export const deletePost = async (req,res) => {
 
   res.json({message: 'Post deleted succefully'});
 }
+
+export const likePost = async (req,res) => {
+  const {id} = req.params
+  
+  if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('NO post with that id');
+
+  const post = await PostMessage.findById(id)
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount + 1}, {new: true})
+
+  res.json(updatedPost)
+
+}
