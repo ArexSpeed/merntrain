718e05ec3eb4b139fcd63d2775fba18877fea344
Arek Cichocki <arexspeed95@gmail.com>
2021-03-08 18:02:33 +0100

    custom hooks

---
+++ todoist/src/components/Tasks.js
@@ -0,0 +1,22 @@
+import { Checkbox } from "./Checkbox";
+
+export const Tasks = () => {
+  const tasks = [];
+  let projectName = ''
+
+  return (
+    <div className="tasks" data-testid="tasks">
+      <h2 data-testid="project-name">{projectName}</h2>
+
+      <ul className="tasks__list">
+        {tasks.map(task => (
+          <li key={`${task.id}`}>
+            <Checkbox />
+            <span>{task.task}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
