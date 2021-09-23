986549c6c083fe64121ef2f40052e6cd3aad4887
Arek Cichocki <arexspeed95@gmail.com>
2021-09-23 14:09:48 +0200

    make crypto info

--- cryptoapp/README.md
+++ cryptoapp/README.md
@@ -3,3 +3,6 @@ project from https://www.youtube.com/watch?v=9DDX3US3kss
 redux toolkit 
 - for getting api to the store - get query -> https://redux-toolkit.js.org/tutorials/rtk-query
 - import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+- useGetCryptosQuery with number count (how many item to get) adding by 
+query: (count) => createRequest(`/coins?limit=${count}`)
+
--- cryptoapp/src/components/Homepage.jsx
+++ cryptoapp/src/components/Homepage.jsx
@@ -1,15 +1,16 @@
 import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
 import millify from 'millify';
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components';
 
 const { Title } = Typography;
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
 
-  
-  if (isFetching) return '...Loading'
+  if (isFetching) return '...Loading';
   return (
     <>
       <Title level={2} className="heading">Global Crypto Stats</Title>
@@ -21,6 +22,16 @@ const Homepage = () => {
         <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
         <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Crypto in the World</Title>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more </Link></Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Lates Crypto News</Title>
+        <Title level={3} className="show-more"><Link to="/news">Show more </Link></Title>
+      </div>
+      <News simplified />
     </>
   )
 }
