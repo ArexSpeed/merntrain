8d448188917f77a3c9bf8d87b82fc9e77e66391c
Arek Cichocki <arexspeed95@gmail.com>
2021-12-07 13:39:43 +0100

    connect, route

--- subscriptionapp/server/src/index.ts
+++ subscriptionapp/server/src/index.ts
@@ -1,11 +1,24 @@
 import express from 'express';
+import authRoutes from './routes/auth';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-})
+dotenv.config();
 
-app.listen(5000, () => {
-  console.log('Server listening');
-});
+
+mongoose.connect(process.env.MONGO_URI as string)
+  .then(() => {
+    console.log("connected to db");
+    app.use(express.json());
+    app.use("/auth", authRoutes);
+    app.listen(5000, () => {
+      console.log('Server listening');
+    });
+  })
+  .catch((error) => {
+    console.log({ error });
+    throw new Error(error);
+  })
+
