9e179bb7beecdeba21117ed3d88385afa7776c2b
Arek Cichocki <arexspeed95@gmail.com>
2021-12-08 14:36:29 +0100

    middleware for token, types for express

---
+++ subscriptionapp/server/src/middleware/checkAuth.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
+  let token = req.header("authorization");
+  
+  if(!token) {
+    return res.status(403).json({
+      errors: [
+        {
+          msg: "unauthorize"
+        }
+      ]
+    })
+  }
+
+  token = token.split(" ")[1];
+
+  try {
+    const user = await jwt.verify(token, process.env.JWT_SECRET as string) as { email: string };
+    req.user = user.email;
+    next();
+  } catch (error) {
+    return res.status(403).json({
+      errors: [
+        {
+          msg: "unauthorize"
+        }
+      ]
+    })
+  }
+
+  //console.log(token);
+
+  //res.send(token);
+}
--- subscriptionapp/server/src/routes/auth.ts
+++ subscriptionapp/server/src/routes/auth.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from 'express-validator';
 import User from "../models/user";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { checkAuth } from "../middleware/checkAuth";
 
 const router = express.Router();
 
@@ -105,6 +106,21 @@ router.post("/login", async (req, res) => {
     errors: [],
     data: {
       token,
+      user: {
+        id: user._id,
+        email: user.email
+      }
+    }
+  })
+});
+
+router.get('/me', checkAuth, async (req, res) => {
+  //res.send(req.user);
+  const user = await User.findOne({ email: req.user });
+
+  return res.json({
+    errors: [],
+    data: {
       user: {
         id: user._id,
         email: user.email
