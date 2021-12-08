5618de667cf309d767df0922a458f2b576de582d
Arek Cichocki <arexspeed95@gmail.com>
2021-12-08 15:05:27 +0100

    context, logout

--- subscriptionapp/client/src/components/Nav/Nav.tsx
+++ subscriptionapp/client/src/components/Nav/Nav.tsx
@@ -1,7 +1,23 @@
-import { Navbar, NavItem, NavLink } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { Navbar, NavItem } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserContext } from '../../context';
+import styled from "styled-components";
+
+const LeftNavContainer = styled.div`
+  margin-left: auto;
+`
 
 const Nav = () => {
+  const [state, setState] = useContext(UserContext);
+  const navigate = useNavigate();
+
+  console.log(state, "nav");
+  const handleLogout = () => {
+    setState({ data: null, loading: false, error: null });
+    localStorage.removeItem("token");
+    navigate("/");
+  }
   return (
     <Navbar>
       <NavItem>
@@ -9,13 +25,18 @@ const Nav = () => {
           Home
         </Link>
       </NavItem>
-      {
-        localStorage.getItem("token") && (
+      {state.data && (
+        <LeftNavContainer>
           <NavItem>
-            <Link to="/" className="nav-link">
+            <Link 
+              to="/" 
+              className="nav-link"
+              onClick={handleLogout}
+            >
               Logout
             </Link>
           </NavItem>
+        </LeftNavContainer>
         )
       }
     </Navbar>
